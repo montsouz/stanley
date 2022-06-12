51e8e0ee31a8aba8076e4b2bfe6847f799b09dc1
montsouz <guilhermesouzamont@gmail.com>
2022-06-12 18:54:04 -0300

    setup prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prisma interferes with NestJS enableShutdownHooks.
+  // Prisma listens for shutdown signals and will call process.exit()
+  // before your application shutdown hooks fire. To deal with this,
+  // you would need to add a listener for Prisma beforeExit event.
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
